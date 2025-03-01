21760bf00043442aee0de52a6d7b5a3d356dcac0
mishablablabla <mzuew2006@gmail.com>
2025-03-01 15:12:26 +0100

    add question & add option

--- src/tests.js
+++ src/tests.js
@@ -1,1 +1,96 @@
-import ('./pages/tests.css')
+import("./pages/tests.css");
+
+window.addEventListener("DOMContentLoaded", () => {
+  const addQuestion = document.querySelector(".add-question"),
+    sectionQuestions = document.querySelector(".questions"),
+    questionElement = document.querySelector(".question");
+
+  sectionQuestions.addEventListener("click", (event) => {
+    addNewOptionInQuestion(event); // add new option in question
+  });
+
+  addQuestion.addEventListener("click", () => {
+    addNewQuestion(); //add new question function
+  });
+
+  function addNewQuestion() {
+    sectionQuestions.insertAdjacentHTML(
+      "beforeend",
+      `    <div class="question">
+          <input
+            class="data_field_question"
+            type="text"
+            placeholder="Question"
+          />
+          <div class="options">
+            <div class="option">
+              <label class="custom-checkbox">
+                <input type="checkbox" id="option" />
+                <span class="checkmark"></span>
+              </label>
+              <input
+                class="data_field_question"
+                type="text"
+                placeholder="option1"
+              />
+            </div>
+            <div class="option">
+              <label class="custom-checkbox">
+                <input type="checkbox" id="option" />
+                <span class="checkmark"></span>
+              </label>
+              <input
+                class="data_field_question"
+                type="text"
+                placeholder="option2"
+              />
+            </div>
+          </div>
+          <button class="add-option">+ Add option</button>
+          <div class="footer-test">
+            <svg
+              class="delete-question"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              style="fill: rgb(231, 147, 203)"
+            >
+              <path
+                d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"
+              ></path>
+            </svg>
+
+            <span class="status_option"
+              >The correct answer is not selected*</span
+            >
+          </div>
+        </div>`
+    );
+  }
+
+  function addNewOptionInQuestion(event) {
+    if (event.target.classList.contains("add-option")) {
+      const parentElement = event.target.closest(".question"),
+        optionList = parentElement.querySelector(".options"),
+        questionCount = optionList.querySelectorAll(".option").length + 1;
+
+      optionList.insertAdjacentHTML(
+        "beforeend",
+        `
+          <div class="option">
+              <label class="custom-checkbox">
+                <input type="checkbox" id="option">
+                <span class="checkmark"></span>
+              </label>
+              <input class="data_field_question" type="text" placeholder="option ${questionCount}">
+            </div>
+        `
+      );
+
+      console.log(optionList);
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,58 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        main: './src/index.js',
-        tests: './src/tests.js' // Добавьте отдельный вход для тестов
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        publicPath: '/',
-    },
-    mode: 'development',
-    devServer: {
-        static: path.resolve(__dirname, './dist'),
-        open: true,
-        compress: true,
-        port: 8080,
-        historyApiFallback: true,
-        watchFiles: ['src/**/*']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader'
-                ]
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            chunks: ['main'], // Указываем, что этот HTML будет использовать только основной вход
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/tests.html',
-            filename: 'tests.html',
-            chunks: ['tests'], // Указываем, что этот HTML будет использовать вход для тестов
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css', // Настройка для именования CSS файлов
-        }),
-    ]
-}
+  entry: {
+    main: "./src/index.js",
+    tests: "./src/tests.js", // Добавьте отдельный вход для тестов
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+    watchFiles: ["src/**/*"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["main"], // Указываем, что этот HTML будет использовать только основной вход
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/tests.html",
+      filename: "tests.html",
+      chunks: ["tests"], // Указываем, что этот HTML будет использовать вход для тестов
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css", // Настройка для именования CSS файлов
+    }),
+  ],
+};
